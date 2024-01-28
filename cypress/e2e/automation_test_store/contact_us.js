9e51567e1d3046e94baf164bbce802f92b200efb
Sloth73 <Pichl.tomas.95@gmail.com>
2024-01-28 19:54:04 +0100

    Refactoring code, better selectors

--- cypress/e2e/automation_test_store/contact_us.js
+++ cypress/e2e/automation_test_store/contact_us.js
@@ -1,14 +1,16 @@
-/// <reference types = "Cypress" />
+///<reference types = "cypress"/>
 
 describe("Test contact Us form via Automation Test Store", () => {
   it("Should be able to submit a succesfull submission via contact us form", () => {
     cy.visit("https://automationteststore.com/");
-    cy.get(".info_links_footer > :nth-child(5) > a").click();
+    cy.get("a[href$='contact']").click();  //$ means ends with
+    // cy.xpath("a[contains(@href, 'contact')]");
     cy.get("#ContactUsFrm_first_name").type("John");
     cy.get("#ContactUsFrm_email").type("john.doe@gmail.com");
     cy.get("#ContactUsFrm_enquiry").type(
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In sem justo, commodo ut, suscipit at, pharetra vitae, orci."
     );
-    cy.get(".col-md-6 > .btn").click();
+    cy.get("button[title='Submit']").click();
+
   });
 });
--- cypress/e2e/webdriver_uni/contact-us.js
+++ cypress/e2e/webdriver_uni/contact-us.js
@@ -1,4 +1,3 @@
-/// <reference types = "Cypress" />
 
 describe("Test contact Us form via WebdriverUni", () => {
     it("Should be able to submit a succesfull submission via contact us form", () => {
