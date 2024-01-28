92abf56fa7767ddd46bfaec001bd4b99b8e08119
Sloth73 <Pichl.tomas.95@gmail.com>
2024-01-28 17:31:32 +0100

    Adding reference types

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,7 @@
+/// <reference types = "Cypress" />
+
+describe("Test contact Us form via WebdriverUni", () => {
+  it("Should be able to submit a succesfull submission via contact us form", () => {});
+
+  it("Should not be able to submit a succesfull submission via contact us form as all fields are required", () => {});
+});
