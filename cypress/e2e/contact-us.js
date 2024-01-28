18f0b3d8bda047524188531406e87d5469263df0
Sloth73 <Pichl.tomas.95@gmail.com>
2024-01-28 17:49:29 +0100

    Identifying contact us form

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,7 +1,10 @@
 /// <reference types = "Cypress" />
 
 describe("Test contact Us form via WebdriverUni", () => {
-  it("Should be able to submit a succesfull submission via contact us form", () => {});
+    it("Should be able to submit a succesfull submission via contact us form", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#contact-us > .thumbnail").click();
+  });
 
   it("Should not be able to submit a succesfull submission via contact us form as all fields are required", () => {});
 });
