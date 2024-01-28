47824fe779c9bf5bd1f67becf36d70d77ba02d2a
Sloth73 <Pichl.tomas.95@gmail.com>
2024-01-28 18:23:26 +0100

    Filling in some data to second test scenario

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -3,7 +3,7 @@
 describe("Test contact Us form via WebdriverUni", () => {
     it("Should be able to submit a succesfull submission via contact us form", () => {
         // cy.visit("http://www.webdriveruniversity.com")
-        // cy.get("#contact-us > .thumbnail").click();
+         // cy.get("#contact-us > .thumbnail").click();
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.get('[name="first_name"]').type("John");
         cy.get('[name="last_name"]').type("Doe");
@@ -15,5 +15,13 @@ describe("Test contact Us form via WebdriverUni", () => {
 
   });
 
-  it("Should not be able to submit a succesfull submission via contact us form as all fields are required", () => {});
+    it("Should not be able to submit a succesfull submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("John");
+        cy.get('[name="email"]').type("john.doe@gmail.com");
+        cy.get("textarea.feedback-input").type(
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In sem justo, commodo ut, suscipit at, pharetra vitae, orci."
+        );
+        cy.get('[type="submit"]').click();
+  });
 });
