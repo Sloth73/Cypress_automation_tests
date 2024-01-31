0f385588a94e006e2f753a65e5dfeb8cef6458b9
Sloth73 <Pichl.tomas.95@gmail.com>
2024-01-31 18:59:58 +0100

    Assertion on url

--- cypress/e2e/webdriver_uni/contact-us.js
+++ cypress/e2e/webdriver_uni/contact-us.js
@@ -6,6 +6,7 @@ describe("Test contact Us form via WebdriverUni", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.document().should("have.property", "charset").and('eq', 'UTF-8');
         cy.title().should("include", "WebDriver | Contact Us");
+      cy.url().should("include", 'contactus');
         cy.get('[name="first_name"]').type("John");
         cy.get('[name="last_name"]').type("Doe");
         cy.get('[name="email"]').type("john.doe@gmail.com");
