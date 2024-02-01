79f912875e8071f4343cb9246c91cdbfbe330fed
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-01 10:42:09 +0100

    Selecting item with index

--- cypress/e2e/automation_test_store/inspect_item.js
+++ cypress/e2e/automation_test_store/inspect_item.js
@@ -1,12 +1,16 @@
 describe("Inspect Automation test store items using chain commands", () => {
-//   it("Click on the first item using item header", () => {
-//     cy.visit("https://automationteststore.com/");
-//     // cy.get(
-//     //   "#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname"
-//     // ).click(); (Very likely to break in the future due to possible changes in class, position, etc.)
-//   });
+  it("Click on the first item using item header", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get(
+      "#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname"
+    ).click(); //(Very likely to break in the future due to possible changes in class, position, etc.)
+  });
     it("Click on the first item using item text", () => {
         cy.visit("https://automationteststore.com/");
         cy.get(".prdocutname").contains("Skinsheen Bronzer Stick").click();
     });
+     it("Click on the first item using item index", () => {
+       cy.visit("https://automationteststore.com/");
+       cy.get(".fixed_wrapper").find(".prdocutname").eq(0);
+     });
 });
