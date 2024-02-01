23db5e19f1501b08f4a856f9823c32fd39e77bc0
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-01 10:21:18 +0100

    Adding new test scenario - inspecting items on webpage

---
+++ cypress/e2e/automation_test_store/inspect_item.js
@@ -0,0 +1,6 @@
+describe("Inspect Automation test store items using chain commands", () => {
+  it("Click on the first item using item header", () => {
+    cy.visit("https://automationteststore.com/");
+    
+  });
+});
