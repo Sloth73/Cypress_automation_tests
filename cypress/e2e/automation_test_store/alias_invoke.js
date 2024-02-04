417aaf829e140689ca5075c56ebcfcca248170c1
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-04 16:45:05 +0100

    Adding some comments

--- cypress/e2e/automation_test_store/alias_invoke.js
+++ cypress/e2e/automation_test_store/alias_invoke.js
@@ -4,8 +4,8 @@ describe("Alias and invoke", () => {
   it("Validate a specific hair care product", () => {
     cy.visit("https://automationteststore.com/");
       cy.get("a[href*='product/category&path']").contains("Hair Care").click();
-      cy.get(".fixed_wrapper .prdocutname").eq(0).invoke('text').as('productThumbnail');
-      cy.get('@productThumbnail').its('length').should('be.gt', 5);
+      cy.get(".fixed_wrapper .prdocutname").eq(0).invoke('text').as('productThumbnail'); // Something like creating variable, that stores informations
+      cy.get('@productThumbnail').its('length').should('be.gt', 5); // When reffering to an alias, always use @
       cy.get("@productThumbnail").should('include', 'Seaweed Conditioner');
     });
   });
