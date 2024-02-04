10b2f2e499db2733c301827d720d27bc07a8c519
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-04 16:21:01 +0100

    Adding a if statement to second test spec - wrap logic - in comment

--- cypress/e2e/automation_test_store/iterate_over_elements.js
+++ cypress/e2e/automation_test_store/iterate_over_elements.js
@@ -3,13 +3,18 @@
 describe("Iterating over elements", () => {
   it("Log information of all hair care products", () => {
     cy.visit("https://automationteststore.com/");
-      cy.get("a[href*='product/category&path']").contains("Hair Care").click();
-      cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
-          cy.log("Index: " + index + " - " + $el.text());
-      })
+    cy.get("a[href*='product/category&path']").contains("Hair Care").click();
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+      cy.log("Index: " + index + " - " + $el.text());
+    });
   });
   it("Add specific product to basket", () => {
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path']").contains("Hair Care").click();
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+      if ($el.text().includes("Curls to straight Shampoo")) {
+        cy.wrap($el).click(); // Wrap is used to yeld the element (from if statement) and enables us to use cypress commands
+      }
+    });
   });
 });
