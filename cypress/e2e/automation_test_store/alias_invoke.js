6a28831e71edf5f10bf897083b4ed51538792fee
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-04 17:20:55 +0100

    Adding new test - validating a number of products

--- cypress/e2e/automation_test_store/alias_invoke.js
+++ cypress/e2e/automation_test_store/alias_invoke.js
@@ -1,11 +1,18 @@
 ///<reference types = "cypress"/>
 
 describe("Alias and invoke", () => {
-  it("Validate a specific hair care product", () => {
-    cy.visit("https://automationteststore.com/");
-      cy.get("a[href*='product/category&path']").contains("Hair Care").click();
-      cy.get(".fixed_wrapper .prdocutname").eq(0).invoke('text').as('productThumbnail'); // Something like creating variable, that stores informations
-      cy.get('@productThumbnail').its('length').should('be.gt', 5); // When reffering to an alias, always use @
-      cy.get("@productThumbnail").should('include', 'Seaweed Conditioner');
+    it("Validate a specific hair care product", () => {
+        cy.visit("https://automationteststore.com/");
+        cy.get("a[href*='product/category&path']").contains("Hair Care").click();
+        cy.get(".fixed_wrapper .prdocutname").eq(0).invoke('text').as('productThumbnail'); // Something like creating variable, that stores informations
+        cy.get('@productThumbnail').its('length').should('be.gt', 5); // When reffering to an alias, always use @
+        cy.get("@productThumbnail").should('include', 'Seaweed Conditioner');
     });
+    it.only("Validate a number of product in specific div element", () => {
+        cy.visit("https://automationteststore.com/");
+        //   cy.get("a[href*='product/category&path']").contains("Hair Care").click();
+        cy.get(".thumbnail").as('productThumbnail')
+        cy.get('@productThumbnail').its('length').should('eq', 16)
+            
+    })
   });
