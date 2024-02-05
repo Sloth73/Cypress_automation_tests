85a8b9d496a45d1fff747326cfeaec52ec8621ac
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-05 19:22:36 +0100

    Adding new test scenario of calculating products prices

--- cypress/e2e/automation_test_store/alias_invoke.js
+++ cypress/e2e/automation_test_store/alias_invoke.js
@@ -8,12 +8,16 @@ describe("Alias and invoke", () => {
         cy.get('@productThumbnail').its('length').should('be.gt', 5); // When reffering to an alias, always use @
         cy.get("@productThumbnail").should('include', 'Seaweed Conditioner');
     });
-    it.only("Validate a number of product in specific div element", () => {
+    it("Validate a number of product in specific div element", () => {
         cy.visit("https://automationteststore.com/");
-        //   cy.get("a[href*='product/category&path']").contains("Hair Care").click();
         cy.get(".thumbnail").as('productThumbnail')
         cy.get('@productThumbnail').its('length').should('eq', 16);
         cy.get("@productThumbnail").find(".productcart").invoke('attr', 'title').should('contain', 'Add to Cart');
             
     })
+    it("Calculate total or normal and sale products", () => {
+      cy.visit("https://automationteststore.com/");
+      cy.get(".thumbnail").as("productThumbnail");
+      
+    });
   });
