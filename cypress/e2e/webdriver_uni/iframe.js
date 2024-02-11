f21244baf17c45890372f680099bdcd24f1aa79d
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 16:09:44 +0100

    Handeling modals and iframes

--- cypress/e2e/webdriver_uni/iframe.js
+++ cypress/e2e/webdriver_uni/iframe.js
@@ -3,7 +3,18 @@
 describe("Handeling iframe and modals", () => {
     it("Handle webdriveruni iframe and modal", () => {
         cy.visit("http://www.webdriveruniversity.com");
-        cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true }); 
-  
+        cy.get("#iframe").invoke("removeAttr", "target").click({ force: true }); 
+
+        cy.get('#frame').then($iframe => {
+            const body = $iframe.contents().find('body');
+            cy.wrap(body).as('iframe');
+        })
+        cy.get('@iframe').find('#button-find-out-more').click() //we can use click, because its wrapped 
+        cy.get("@iframe").find("#myModal").as('modal');
+        cy.get('@modal').should(($expectedText) => {
+            const text = $expectedText.text()
+            expect(text).to.include('Welcome to');
+        })
+        cy.get('@modal').contains('Close').click()
     })
 })
