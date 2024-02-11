55e96a79e2f9dcd9fc96f3434ead27332c2dbae9
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 16:29:07 +0100

    unchecking boxes

--- cypress/e2e/webdriver_uni/checkboxes.js
+++ cypress/e2e/webdriver_uni/checkboxes.js
@@ -1,9 +1,21 @@
 ///<reference types = "cypress"/>
 
 describe("Verify checkboxes via webdriveruni", () => {
-  it("HCheck and validate checkbox", () => {
+  it("Check and validate checkbox", () => {
     cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click({ force: true });
-cy.get("#checkboxes > :nth-child(1) > input").check();
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    //   cy.get("#checkboxes > :nth-child(1) > input").check();
+    //   cy.get("#checkboxes > :nth-child(1) > input").check().should('be.checked');
+      cy.get("#checkboxes > :nth-child(1) > input").as('option01')
+          cy.get('@option01').check().should('be.checked');
   });
+    it("Uncheck and validate checkbox", () => {
+      cy.visit("http://www.webdriveruniversity.com");
+      cy.get("#dropdown-checkboxes-radiobuttons")
+        .invoke("removeAttr", "target")
+          .click({ force: true });
+        cy.get(":nth-child(5) > input").uncheck().should('not.be.checked');
+    });
 });
