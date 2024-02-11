c21f260f19ec7d0f8ea54b63a651b78f0aedba07
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 14:26:39 +0100

    new test added - origin command

--- cypress/e2e/other/same_origin_policy.js
+++ cypress/e2e/other/same_origin_policy.js
@@ -1,7 +1,7 @@
 ///<reference types = "cypress"/>
 
 describe("Cypress web security", () => {
-  it("Validate visiting two different domains", () => {
+  it.skip("Validate visiting two different domains", () => {
     cy.visit("https://www.webdriveruniversity.com/");
     cy.visit("https://automationteststore.com/");
     // This code will fail due to security mesures, can't visit two different superdomains in the same test
@@ -11,5 +11,13 @@ describe("Cypress web security", () => {
     cy.visit("https://www.webdriveruniversity.com/");
     cy.get("#automation-test-store").invoke("removeAttr", "target").click();
     // This code will also fail due to same resons, but if we disable chrome web security - cypress-config.js -> chromeWebSecurity : false - this code will execute
+  });
+  it("Origin command", () => {
+    cy.origin("http://www.webdriveruniversity.com", () => {
+      cy.visit("/");
+    });
+    cy.origin("https://www.automationteststore.com", () => {
+      cy.visit("/");
+    });
   });
 });
