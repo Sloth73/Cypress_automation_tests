52a7503fc68838f2367ab2109c8cd80ed586bb88
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 18:12:10 +0100

    Perfoming mousedown test with background color asertion

--- cypress/e2e/webdriver_uni/mouse_actions.js
+++ cypress/e2e/webdriver_uni/mouse_actions.js
@@ -16,4 +16,11 @@ describe("Test mouse actions", () => {
        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
          cy.get('#double-click').dblclick();
      });
+    it("I should be able to hold down the left mouse click button on a given element", () => {
+        cy.visit("http://www.webdriveruniversity.com");
+        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
+        cy.get('#click-box').trigger('mousedown', { which: 1 }).then(($el) => {
+            expect($el).to.have.css('background-color', 'rgb(0, 255, 0)')
+        })
+    });
 });
