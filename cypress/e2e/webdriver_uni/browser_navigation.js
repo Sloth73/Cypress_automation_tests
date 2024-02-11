3caa71c3c5f396edd36d6493f60276a22e21b6f1
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 14:40:57 +0100

    Adding some browser actions

--- cypress/e2e/webdriver_uni/browser_navigation.js
+++ cypress/e2e/webdriver_uni/browser_navigation.js
@@ -4,7 +4,11 @@ describe("Validate WebdriverUni homepage links", () => {
   it("Confirm lins redirect to be the correct pages", () => {
     cy.visit("http://www.webdriveruniversity.com");
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true }); //Cypress doesn't support multi-tabs. This jQuery command removes attribute that calls the second "blank" page and shows desired content in original tab
-    cy.url().should("include", "contuctus");
+    cy.url().should("include", "contactus");
     cy.go("back"); //cy.go enables us to perform navigation actions
+    cy.reload();
+    //cy.reload('true') - reload without using cache
+      cy.go('forward')
+      cy.url().should("include", "contactus");
   });
 });
