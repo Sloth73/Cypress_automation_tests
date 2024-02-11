25216b6484509745ddf1e868e989982a5d7392fc
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 14:56:39 +0100

    Adding asertion

--- cypress/e2e/webdriver_uni/browser_navigation.js
+++ cypress/e2e/webdriver_uni/browser_navigation.js
@@ -16,5 +16,6 @@ describe("Validate WebdriverUni homepage links", () => {
       cy.go('back');
       cy.url().should("include", "http://www.webdriveruniversity.com/");
       cy.get("#to-do-list").invoke("removeAttr", "target").click({ force: true });
+      cy.url().should("include", "To-Do-List");
   });
 });
