38ca1c0d1c876af516c1a37fee833f1d733f724e
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 17:40:22 +0100

    Autocomplete selection - Grapes

--- cypress/e2e/webdriver_uni/autocomplete_dropdown_list.js
+++ cypress/e2e/webdriver_uni/autocomplete_dropdown_list.js
@@ -6,7 +6,32 @@ describe("Verify autocomplete dropdown list via webdriveruni", () => {
     cy.get("#autocomplete-textfield")
       .invoke("removeAttr", "target")
         .click({ force: true });
-      cy.get('#myInput').type('A')
+      cy.get('#myInput').type('A');
+      cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+          const product = $el.text();
+          const productToSelect = 'Avacado';
+          if (product === productToSelect) {
+              $el.click();
+              cy.get('#submit-button').click()
+              cy.url().should('include', productToSelect);
+          }
+      })
     
   });
+    it("Select Grape via autocomplete list", () => {
+      cy.visit("http://www.webdriveruniversity.com");
+      cy.get("#autocomplete-textfield")
+        .invoke("removeAttr", "target")
+        .click({ force: true });
+      cy.get("#myInput").type("G");
+      cy.get("#myInputautocomplete-list > *").each(($el, index, $list) => {
+        const product = $el.text();
+        const productToSelect = "Grapes";
+        if (product === productToSelect) {
+          $el.click();
+          cy.get("#submit-button").click();
+          cy.url().should("include", productToSelect);
+        }
+      });
+    });
 });
