ad9170f74e6506bab668c1332d3822def823abe0
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 14:00:53 +0100

    Creating new test scenario - cypress web security

---
+++ cypress/e2e/other/same_origin_policy.js
@@ -0,0 +1,11 @@
+///<reference types = "cypress"/>
+
+describe("Cypress web security", () => {
+  it("Validate visiting two different domains", () => {
+    
+  });
+
+  it("", () => {
+    
+  });
+});
