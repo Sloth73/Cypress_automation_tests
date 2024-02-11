7b48faec4ad82649d5fbfa0f0864dbe56703cc32
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 14:14:29 +0100

    Adding some comments - disabeling chrome web security

--- cypress/e2e/other/same_origin_policy.js
+++ cypress/e2e/other/same_origin_policy.js
@@ -10,6 +10,6 @@ describe("Cypress web security", () => {
   it("Validate visiting two different domains via user action", () => {
     cy.visit("https://www.webdriveruniversity.com/");
     cy.get("#automation-test-store").invoke("removeAttr", "target").click();
-    // This code will also fail due to same resons
+    // This code will also fail due to same resons, but if we disable chrome web security - cypress-config.js -> chromeWebSecurity : false - this code will execute
   });
 });
