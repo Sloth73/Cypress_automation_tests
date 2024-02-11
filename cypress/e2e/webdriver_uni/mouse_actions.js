aef28e50f257dffbd4e57cbdfbe2706d9b766357
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 18:06:01 +0100

    Performing a doubleckick test

--- cypress/e2e/webdriver_uni/mouse_actions.js
+++ cypress/e2e/webdriver_uni/mouse_actions.js
@@ -10,5 +10,10 @@ describe("Test mouse actions", () => {
         cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
         cy.get('#draggable').trigger('mousedown', { which: 1 });
         cy.get('#droppable').trigger('mousemove').trigger('mouseup', {force: true})
+    });
+     it("I should be able perform the double mouse click", () => {
+       cy.visit("http://www.webdriveruniversity.com");
+       cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
+         cy.get('#double-click').dblclick();
      });
 });
