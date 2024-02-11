8aa9afd305e2c6527df4ece9a6bbfa0ccb171ee0
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 16:58:25 +0100

    Validating radiobuttons

--- cypress/e2e/webdriver_uni/radiobuttons.js
+++ cypress/e2e/webdriver_uni/radiobuttons.js
@@ -5,8 +5,17 @@ describe("Verify radiobuttons via webdriveruni", () => {
     cy.visit("http://www.webdriveruniversity.com");
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
-        .click({ force: true });
-      cy.get('#radio-buttons').find("[type='radio']").first().check();
-      cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
+      .click({ force: true });
+    cy.get("#radio-buttons").find("[type='radio']").first().check();
+    cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
+  });
+  it("Validate the states of specific radiobuttons", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("[value = 'lettuce']").should("not.be.checked");
+    cy.get("[value = 'cabbage']").should("be.disabled");
+    cy.get("[value = 'pumpkin']").should("be.checked");
   });
 });
