4802c3a2e55ec164a659ed8f2f52c3356a18ad21
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 15:23:34 +0100

    handeling confirm alert manually

--- cypress/e2e/webdriver_uni/js_alerts.js
+++ cypress/e2e/webdriver_uni/js_alerts.js
@@ -9,4 +9,16 @@ describe("Handle js alerts", () => {
           expect(str).to.eq('I am an alert box!')
       })
   });
+    it("Validate js confirm alert box works correctly when clicking ok", () => {
+      cy.visit("http://www.webdriveruniversity.com");
+      cy.get("#popup-alerts")
+        .invoke("removeAttr", "target")
+        .click({ force: true });
+      cy.get("#button4").click();
+        cy.on('window:alert', (str) => { 
+            return true;
+        })
+        cy.get("#confirm-alert-text").contains("You pressed OK!");
+      
+    });
 });
