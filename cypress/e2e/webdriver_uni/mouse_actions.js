fc0b32de66c0d5bf8b2bc2464e1d9d789eff3e6c
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 18:03:26 +0100

    Drag and drop mouse action

--- cypress/e2e/webdriver_uni/mouse_actions.js
+++ cypress/e2e/webdriver_uni/mouse_actions.js
@@ -4,6 +4,11 @@ describe("Test mouse actions", () => {
   it("Scroll element into view", () => {
     cy.visit("http://www.webdriveruniversity.com");
     cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
-
   });
+    it("I should be able to drag and drop a dragable item", () => {
+        cy.visit("http://www.webdriveruniversity.com");
+        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({ force: true });
+        cy.get('#draggable').trigger('mousedown', { which: 1 });
+        cy.get('#droppable').trigger('mousemove').trigger('mouseup', {force: true})
+     });
 });
