a69c5ecf5b8247c255250d8a3ec378978b4e6052
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-11 15:10:43 +0100

    handeling alert event manually

--- cypress/e2e/webdriver_uni/js_alerts.js
+++ cypress/e2e/webdriver_uni/js_alerts.js
@@ -1,9 +1,12 @@
 ///<reference types = "cypress"/>
 
 describe("Handle js alerts", () => {
-    it("Confirm js alerts contains the correct text", () => {
-        cy.visit("http://www.webdriveruniversity.com");
-        cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true }); 
-
-    });
+  it("Confirm js alerts contains the correct text", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click({ force: true });
+      cy.get("#button1").click();
+      cy.on('window:alert', (str) => {
+          expect(str).to.eq('I am an alert box!')
+      })
+  });
 });
