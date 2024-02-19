7f7a610577355eefb6a42c1909350e36c99977b5
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-19 17:13:59 +0100

    Uploading file form folder

--- cypress/e2e/webdriver_uni/file_upload.js
+++ cypress/e2e/webdriver_uni/file_upload.js
@@ -3,6 +3,8 @@
 describe('Test file upload via webdriver uni', () => {
     it('Uploading file', () => {
       cy.visit("http://www.webdriveruniversity.com");
-      cy.get("#file-upload").invoke("removeAttr", "target").click({ force: true });   
+        cy.get("#file-upload").invoke("removeAttr", "target").click({ force: true });  
+        cy.get('#myFile').selectFile('cypress/fixtures/laptop.png');
+        cy.get('#submit-button').click();
     });
 });
