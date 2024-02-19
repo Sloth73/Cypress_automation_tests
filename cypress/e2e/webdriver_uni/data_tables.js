8ed85b3d73bfe31b035e9ad56f17d7b1e479ac00
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-19 14:02:41 +0100

    logging table data via for loop

--- cypress/e2e/webdriver_uni/data_tables.js
+++ cypress/e2e/webdriver_uni/data_tables.js
@@ -1,12 +1,19 @@
 /// <reference types="Cypress" />
+
 describe("Handeling data via webdriveruni", () => {
   beforeEach(() => {
     cy.visit("http://webdriveruniversity.com/");
     cy.get("#data-table").invoke("removeAttr", "target").click({ force: true });
   });
-  it("Calculate and assert the age of all users", () => {
+    it("Calculate and assert the age of all users", () => {
+        const userDetails = [];
       cy.get('#thumbnail-1 td').each(($el, i, $list) => {
-    
+          userDetails[i] = $el.text();
+      }).then(() => {
+          let i;
+          for (i = 0; i < userDetails.length; i++){
+              cy.log(userDetails[i]);
+          }
 })
   });
 });
