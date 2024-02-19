4b2e6b6b3ece3b961feb6d1831123b1d3177e5c8
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-19 17:03:10 +0100

    Selecting future day, month and year on the date picker via manual selecting date

--- cypress/e2e/webdriver_uni/datepicker.js
+++ cypress/e2e/webdriver_uni/datepicker.js
@@ -3,14 +3,45 @@
 describe("Test datepicker via webdriveruni", () => {
   it("Select date from datepicker", () => {
     cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#datepicker").invoke("removeAttr", "target").click({ force: true });
+      cy.get("#datepicker").invoke("removeAttr", "target").click({ force: true });
+      cy.get('#datepicker').click();
+
+    // let date = new Date();
+    // date.setDate(date.getDate()); //get current date
+    // cy.log(date.getDate());
+
+    // let date1 = new Date();
+    // date1.setDate(date.getDate() + 5); //get current date + 5
+    // cy.log(date1.getDate());
 
     let date = new Date();
-    date.setDate(date.getDate()); //get current date
-    cy.log(date.getDate());
+      date.setDate(date.getDate() + 365);
 
-    let date1 = new Date();
-    date1.setDate(date.getDate() + 5); //get current date + 5
-    cy.log(date1.getDate());
+      let futureYear = date.getFullYear();
+      let futureMonth = date.toLocaleString('default', { month: 'long' });
+      let futureDay = date.getDate();
+
+      cy.log(futureYear, futureMonth, futureDay);
+
+      function selectMonthAndYear() {
+          cy.get('.datepicker-dropdown').find('.datepicker-switch').first().then(currentDate => {
+              if (!currentDate.text().includes(futureYear)) {
+                  cy.get('.next').first().click()
+                  selectMonthAndYear()
+              }
+          }).then(() => {
+              cy.get(".datepicker-dropdown").find(".datepicker-switch").first().then((currentDate) => {
+                  if (!currentDate.text().includes(futureMonth)) {
+                    cy.get(".next").first().click();
+                    selectMonthAndYear();
+                  }
+                });
+          })
+      }
+      function selectFutureDay() {
+          cy.get('[class="day"]').contains(futureDay).click();
+      }
+    //   selectMonthAndYear();
+    //   selectFutureDay()
   });
 });
