c8249ee44defdec7db619252e2b94252b4045cc6
Sloth73 <Pichl.tomas.95@gmail.com>
2024-02-19 16:23:25 +0100

    Selecting current date

--- cypress/e2e/webdriver_uni/datepicker.js
+++ cypress/e2e/webdriver_uni/datepicker.js
@@ -3,7 +3,14 @@
 describe("Test datepicker via webdriveruni", () => {
   it("Select date from datepicker", () => {
     cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#iframe").invoke("removeAttr", "target").click({ force: true });
+    cy.get("#datepicker").invoke("removeAttr", "target").click({ force: true });
 
+    let date = new Date();
+    date.setDate(date.getDate()); //get current date
+    cy.log(date.getDate());
+
+    let date1 = new Date();
+    date1.setDate(date.getDate() + 5); //get current date + 5
+    cy.log(date1.getDate());
   });
 });
